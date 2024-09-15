fb1d2fc8d79378a190caa23220b1348c52260a94
Jacob Robinson <robinson.jacob.e@gmail.com>
2024-09-15 21:37:20 +0100

    Removed BlurredSVG due to Firefox issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import BlurredSVG from "../components/BlurredSVG";
 import SearchInput from "../components/SearchInput";
 
 export default function Home() {
   return (
-    <main
-      style={{
-        background:
-          "linear-gradient(135deg, #1A1B2A 0%, #2B2D42 40%, #3E4157 100%)",
-      }}
-      className="relative flex min-h-screen flex-col items-center justify-between p-24"
-    >
+    <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
       <video
         className="absolute top-0 left-0 w-screen h-screen object-cover opacity-75 z-10"
         src="/WeebWatch.mp4"
@@ -27,10 +20,13 @@ export default function Home() {
         </div>
       </div>
 
-      {/* SVG Overlay */}
-      <div className="absolute top-0 left-0 w-screen h-screen z-20 pointer-events-none">
-        <BlurredSVG />
-      </div>
+      <div
+        style={{
+          background:
+            "linear-gradient(135deg, #1A1B2A 0%, #2B2D42 40%, #3E4157 100%)",
+        }}
+        className="absolute w-screen h-screen top-0 z-20 opacity-70"
+      ></div>
     </main>
   );
 }
