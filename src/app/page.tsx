235c172c959bc805eb97494ac2d9e9c87d3f21b5
Jacob Robinson <robinson.jacob.e@gmail.com>
2024-10-08 01:31:24 +0100

    Added rand video selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import SearchInput from "../components/SearchInput";
 
+const videos = ["/WeebWatch.mp4", "/WeebWatch2.mp4"];
+
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-between p-24">
       <video
         className="absolute top-0 left-0 w-screen h-screen object-cover opacity-75 z-10"
-        src="/WeebWatch.mp4"
+        src={videos[Math.floor(Math.random() * videos.length)]}
         muted
         loop
         autoPlay
